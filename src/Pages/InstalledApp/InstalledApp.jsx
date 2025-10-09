6e7b8ac2255e876440a899653c8a5958693c8f17
RupomPB <rupombadhan111@gmail.com>
2025-10-10 02:46:32 +0600

    added responsive

--- src/Pages/InstalledApp/InstalledApp.jsx
+++ src/Pages/InstalledApp/InstalledApp.jsx
@@ -9,16 +9,16 @@ const InstalledApp = ({ installedApp, handleUninstall }) => {
     <div >
         <div>
 
-      <div className="flex justify-between bg-white gap-11 m-5 rounded-3xl ">
-        <div className="flex">
+      <div className="flex flex-col justify-center items-center md:flex-row md:justify-between bg-white gap-4 md:gap-11 m-3 md:m-5 rounded-3xl shadow-md ">
+        <div className="flex flex-col items-center  md:flex-row ">
             <div className="flex items-center justify-center p-3">
           <img className="w-20 h-20 " src={image}></img>
         </div>
 
-        <div className=" w-70 flex flex-col gap-4 p-3">
+        <div className="  flex items-center md:items-start  flex-col  gap-4 p-3">
           <h1 className=" font-bold text-xl">{title}</h1>
 
-          <div className="  flex justify-around items-center ">
+          <div className="  flex sm:flex-row sm:justify-around justify-around items-center  gap-5">
             <div className="flex gap-1.5 text-green-400 font-bold">
                 <img src={download}></img>
                 <p >{downloads}</p>
--- src/Pages/Recharts/Recharts.jsx
+++ src/Pages/Recharts/Recharts.jsx
@@ -7,12 +7,12 @@ const Recharts = ({ratings}) => {
     // console.log(ratings)
     return (
         
-        <div style={{width:'100%', height:600}} className=' max-w-7xl mx-auto'>
+        <div style={{width:'100%', height:600}} className=' max-w-7xl mx-auto w-full h-[300px] md:h-[500px] lg:h-[600px]'>
 
             <ResponsiveContainer width='100%' height='100%'>
 
 
-            <BarChart data={ratings} layout='vertical' >
+            <BarChart data={ratings}  layout='vertical' >
             
                 <XAxis type='number'  ></XAxis>
                 <YAxis type='category' dataKey='name' ></YAxis>
